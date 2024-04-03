b110ac4ca7a8c48c07e48130aadfbb6be108f173
MoozIiSP <9022638+MoozIiSP@users.noreply.github.com>
2024-04-04 00:17:11 +0800

    u

--- fetch-pkgs.sh
+++ fetch-pkgs.sh
@@ -5,12 +5,12 @@ git clone --depth 1 https://github.com/MoozIiSP/linux-uconsole-rpi4.git linux-uc
 git clone --depth 1 https://github.com/melentye/alarm-pkgbuilds.git alarm-pkgbuilds
 
 # 从linux-uconsole-rpi4仓库复制目录
-cp -r linux-uconsole-rpi4/PKGBUILDs/linux-clockworkpi-uc4 $PKGS_DIR/linux-clockworkpi-uc4
-cp -r linux-uconsole-rpi4/PKGBUILDs/uconsole-4g-cm4 $PKGS_DIR/uconsole-4g-cm4
-cp -r linux-uconsole-rpi4/PKGBUILDs/uconsole-cm4-post-install $PKGS_DIR/uconsole-cm4-post-install
+cp -r linux-uconsole-rpi4/PKGBUILDs/linux-clockworkpi-uc4 pkgs/linux-clockworkpi-uc4
+cp -r linux-uconsole-rpi4/PKGBUILDs/uconsole-4g-cm4 pkgs/uconsole-4g-cm4
+cp -r linux-uconsole-rpi4/PKGBUILDs/uconsole-cm4-post-install pkgs/uconsole-cm4-post-install
 
 # 从alarm-pkgbuilds仓库复制目录
-cp -r alarm-pkgbuilds/core/gcc $PKGS_DIR/gcc
-cp -r alarm-pkgbuilds/core/glibc $PKGS_DIR/glibc
+cp -r alarm-pkgbuilds/core/gcc pkgs/gcc
+cp -r alarm-pkgbuilds/core/glibc pkgs/glibc
 
 echo "All packages fetched successfully."
