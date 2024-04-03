8b076e431a4f34e42abc4f9af3c0ae23fc180ae5
MoozIiSP <9022638+MoozIiSP@users.noreply.github.com>
2024-04-03 22:09:02 +0800

    u

--- fetch-pkgs.sh
+++ fetch-pkgs.sh
@@ -1,16 +1,16 @@
-mkdir -p $PKGS_DIR
+# 克隆MoozIiSP/linux-uconsole-rpi4仓库
+git clone --depth 1 https://github.com/MoozIiSP/linux-uconsole-rpi4.git linux-uconsole-rpi4
 
-#git clone --depth 1 https://github.com/melentye/alarm-pkgbuilds pkgs/
-# using svn to fetch custom PKGBUILDs
-svn export https://github.com/MoozIiSP/linux-uconsole-rpi4/branches/rpi-6.6.y/PKGBUILDs/linux-clockworkpi-uc4 \
-    $PKGS_DIR/linux-clockworkpi-uc4
-svn export https://github.com/MoozIiSP/linux-uconsole-rpi4/branches/rpi-6.6.y/PKGBUILDs/uconsole-4g-cm4 \
-    $PKGS_DIR/uconsole-4g-cm4
-svn export https://github.com/MoozIiSP/linux-uconsole-rpi4/branches/rpi-6.6.y/PKGBUILDs/uconsole-cm4-post-install \
-    $PKGS_DIR/uconsole-cm4-post-install
-svn export https://github.com/melentye/alarm-pkgbuilds/tree/master/core/gcc \
-    $PKGS_DIR/gcc
-svn export https://github.com/melentye/alarm-pkgbuilds/tree/master/core/glibc \
-    $PKGS_DIR/glibc
+# 克隆melentye/alarm-pkgbuilds仓库
+git clone --depth 1 https://github.com/melentye/alarm-pkgbuilds.git alarm-pkgbuilds
+
+# 从linux-uconsole-rpi4仓库复制目录
+cp -r linux-uconsole-rpi4/PKGBUILDs/linux-clockworkpi-uc4 $PKGS_DIR/linux-clockworkpi-uc4
+cp -r linux-uconsole-rpi4/PKGBUILDs/uconsole-4g-cm4 $PKGS_DIR/uconsole-4g-cm4
+cp -r linux-uconsole-rpi4/PKGBUILDs/uconsole-cm4-post-install $PKGS_DIR/uconsole-cm4-post-install
+
+# 从alarm-pkgbuilds仓库复制目录
+cp -r alarm-pkgbuilds/core/gcc $PKGS_DIR/gcc
+cp -r alarm-pkgbuilds/core/glibc $PKGS_DIR/glibc
 
 echo "All packages fetched successfully."
